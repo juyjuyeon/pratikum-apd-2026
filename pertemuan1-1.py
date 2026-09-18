38379025d2585d7f25ed667059b96526937f1ff8
juyjuyeon <wana66497@gmail.com>
2026-09-18 19:42:42 +0700

    update tugas pratikum

--- pertemuan1-1.py
+++ pertemuan1-1.py
@@ -1,5 +1,50 @@
-a = 5
-b = 10
-c = a + b
-print(c)
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
 
+# a = 5
+# b = 10
+# c = a / b
+# print(c)
+
+# print('''halo saya rendy
+# saya suka belajar coding
+# hari ini saya belajar bahasa pemrograman phthon''') 
+
+# teks = "rahmat flowchart"
+
+# #menampilkan index ke-3
+# print(teks[3])
+# #menampilkan index ke-3 hingga ke-6
+# print(teks[3:6])
+# #menampilkan seluruhnya, tetapi dimulai dari index ke-7
+# print(teks[7:])
+# #menampilkan hanya sampai index ke-6
+# print(teks[:6])
+
+# mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])ru
+
+a = "20"
+print(type(a))
+
+nama = input("Masukan nama anda: ")
+print(nama)
+
+a = 26 
+b = 5 
+c = a * b 
+print(c) 
+
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+
+if 20 < 10 or 20 > 10 :
+    print("benar bang") 
+else: 
+    print("salah bang")
