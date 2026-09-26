71badcd5d92be5c39c8c6b7d10d8d1b074fb5144
juyjuyeon <wana66497@gmail.com>
2026-09-26 16:14:17 +0800

    menambahkan folder posttest 3

--- kelas/pertemuan-3/posttest/posttest-apd-3/2609106022_WILIANA_ERIANTI_PT-3.py
+++ kelas/pertemuan-3/posttest/posttest-apd-3/2609106022_WILIANA_ERIANTI_PT-3.py
@@ -1,5 +1,4 @@
 
-print("Nota Transaksi BBM")
 nama_panggilan = input("Nama Panggilan : ")
 nim = input("NIM : ")
 
@@ -8,33 +7,63 @@ if nama_panggilan == "wili" and nim == "22":
     print("1. Pertalite = 10.000")
     print("2. Pertamax = 12.500")
     print("3. Pertamax Turbo = 15.000")
-else: 
-    print("Login Gagal, Silahkan coba lagi")
 
-pilihan_bbm = input("Masukkan pilihan BBM (1/2/3): ")
-jumlah_liter = float(input("Jumlah Liter: "))
+    pilihan_bbm = input("Masukkan pilihan BBM (1/2/3): ")
+    jumlah_liter = float(input("Jumlah Liter: "))   
 
-if pilihan_bbm == "1":
-    harga_bbm_per_liter = 10000
-elif pilihan_bbm == "2":
-    harga_bbm_per_liter = 12500  
-elif pilihan_bbm == "3":
-    harga_bbm_per_liter = 15000
+    if pilihan_bbm == "1":
+        harga_bbm_per_liter = 10000
+    elif pilihan_bbm == "2":
+        harga_bbm_per_liter = 12500
+    elif pilihan_bbm == "3":
+        harga_bbm_per_liter = 15000
+    else:
+        print("Pilihan BBM tidak valid.")
+
+        Total_Harga = harga_bbm_per_liter * jumlah_liter
+
+        if jumlah_liter >= 10:
+            Diskon = Total_Harga * 0.10          
+        elif jumlah_liter >= 5:
+            Diskon = Total_Harga * 0.05
+        else:
+            Diskon = 0  
+
+        Status_Keanggotaan = input("Status Keanggotaan (Ya/Tidak): ")
+        if  Status_Keanggotaan == "Ya":
+            Status_Keanggotaan = "Member"
+            Diskon_member = Total_Harga * 0.02
+        else:
+            Status_Keanggotaan = "Non-Member"
+            Diskon_member = 0
+        Total_Diskon = Diskon + Diskon_member
+        Total_Harga = Total_Harga - Total_Diskon
+
+        Total_Bayar = Total_Harga - Diskon 
+        print("\n--- Total Pembayaran ---")
+        print(f"Total Harga : Rp{int(Total_Harga):,}".replace(",", "."))
+        print(f"Diskon      : Rp{int(Diskon):,}".replace(",", "."))
+        print(f"Total Bayar : Rp{int(Total_Bayar):,}".replace(",", "."))
+
 else:
-    print("Pilihan BBM tidak valid.")
+    print("Login Gagal, Silahkan coba lagi") 
 
-if harga_bbm_per_liter > 0:
-    Total_Harga = harga_bbm_per_liter * jumlah_liter
-    
-if jumlah _liter >= 10:
-    Diskon = Total_Harga * 0.1  # Misalnya diskon 10%   
-elif jumlah_liter >= 5:
-    Diskon = Total_Harga * 0.2  # Misalnya diskon 20%
-else:
-    Diskon = 0  
-    print("Tidak ada diskon untuk pembelian kurang dari 5 liter.")
-    
+garis = "-" * 88
 
-Total_Harga = harga_bbm_per_liter * jumlah_liter
-Diskon = Total_Harga * 0.1  # Misalnya diskon 10%
-Total_Bayar = Total_Harga - Diskon
+print("\n" + "=" * 88)
+print("                                 STRUK TRANSAKSI BBM                                  ")
+print("=" * 88)
+print(f" Nama_Panggilan  : {nama_panggilan}")
+print(f" NIM           : {nim}")
+print(f" Status Member : {Status_Keanggotaan}")
+print(garis)
+print(f"| {'Pilihan_BBM':<15} | {'Harga/L':<10} | {'Liter':<7} | {'Total Awal':<12} | {'Total Diskon':<12} | {'Total Bayar':<12} |")
+print(garis)
+print(f"| {pilihan_bbm:<15} | Rp{harga_bbm_per_liter:<8} | {jumlah_liter:<7.1f} | Rp{Total_Harga:<10.0f} | Rp{Total_Diskon:<10.0f} | Rp{Total_Bayar:<10.0f} |")
+print(garis)
+print(" Rincian Diskon:")
+print(f" - Diskon Pembelian : Rp {Diskon:,.0f}")
+print(f" - Diskon Member    : Rp {Diskon_member:,.0f}")
+print("=" * 88)
+
+
